cef2573e35bbb4799c052978f0ffa9966ccbe957
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-02-28 02:38:04 +0500

    Add files via upload

---
+++ Chapter 1 (Alert)/app.js
@@ -0,0 +1,23 @@
+
+
+// Question 1
+var Heading = "Hello World";
+alert(Heading)
+
+// Question 2
+var Question2 = "Error! Please Enter a valid password"
+alert(Question2)
+
+// Question 3
+var Question3 = "Welcome to js land...\nHappy coding"
+alert(Question3)
+
+// Question 4
+var Question4_1 ="Welcome to JS Land..."
+var Question4_2 ="Happy Coding\nPrevert this page from creating additional dialogs"
+alert(Question4_1)
+alert(Question4_2)
+
+// Question 5
+console.log("Hello... I can run JS through my web browser's console")
+
