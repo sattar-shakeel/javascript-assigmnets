0925fff09ac2d5585b2d30ae7ec7e7459cc2d3b7
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-01 02:07:28 +0500

    Add files via upload

---
+++ Chapter 3 (Variable for Number)/app.js
@@ -0,0 +1,18 @@
+
+// Question 1
+// var age = 20
+// alert("I am " + age + " years old")
+
+// Question 2
+// not cleared
+
+// Question 3
+// var BirthYear = 2003
+// document.write("My birth year is " + BirthYear )
+
+// question 4
+// var visitorName = "Sattar Shakeel"
+// var ProductTitle = "T-Shirt"
+// var Quantity = 5
+// document.write(visitorName + " order " + Quantity +   ProductTitle + " on XYZ Clothing Store")
+
