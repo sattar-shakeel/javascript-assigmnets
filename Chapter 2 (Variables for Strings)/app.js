9c1bf1bffb7bf86b8146f564aa0aecade6a40afa
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-01 02:07:15 +0500

    Add files via upload

---
+++ Chapter 2 (Variables for Strings)/app.js
@@ -0,0 +1,51 @@
+
+
+// CHAPTER 2 (VARIABLE FOR STRING)
+
+// Question 1
+// var name = "Sattar"
+// alert(name)
+
+
+// Question 2
+// var myName;
+// myName = "Sattar Shakeel"
+// alert (myName)
+
+
+// Question 3
+// var titleMessage;
+// titleMessage = "Hello World"
+// alert(titleMessage)
+
+// Question 4
+// var name = "Sattar Shakeel"
+// alert(name)
+// var Age = "20"
+// alert(Age)
+// var education = "Certified Mobile App Development"
+// alert(education)
+
+
+// Questioon 5
+// var name = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+// alert(name)
+
+
+// Question 6
+// var email = "My email address is sattarmarkatiya16@gmail.com"
+// alert(email)
+
+
+// Question 7
+// var book = "A smarter Way to learn JavaScript"
+// alert("I am trying to learn from the book " + book)
+
+
+// Question 8
+// document.write("Yah! I can write HTML content through JavaScript")
+
+
+// // Question 9
+// var specialStrings = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(specialStrings)
