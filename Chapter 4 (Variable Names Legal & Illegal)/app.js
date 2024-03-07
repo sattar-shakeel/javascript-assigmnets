e0f071b1b9712562b4b40cb3d1301700cc38c9a3
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-08 00:42:03 +0500

    Add files via upload

---
+++ Chapter 4 (Variable Names Legal & Illegal)/app.js
@@ -0,0 +1,46 @@
+
+// // Question 1
+
+// var myName = "sattar"
+// var my_age = 20
+// var $education = "intermediate"
+
+
+// Question 2
+
+// Legal names
+
+// 1. myName
+// 2. user_input
+// 3. $myEducation
+// 4. Quantity123
+// 5. _finalAmount
+
+// Illegal names
+
+// 1. 12name
+// 2. var
+// 3. name
+// 4. !specialcharecter
+// 5. @225
+
+
+
+
+// Question 3
+let point1_1 = "Number"
+let point1_2 = "$"
+let point1_3 = "_"
+let point1_4 = "Letter"
+
+
+document.write("<h1>Rules for naming JS variables</h1>")
+document.write("Variable names can only contain " , point1_1  + " , " + point1_2 + " , " + point1_3 + " For example $my_1stVariable" + " <br> ")
+document.write("Variables must begin with a " , point1_4  + " , " + point1_2 + " or " + point1_3 + " For example $name, _name or name" + "<br>")
+document.write("Variable names are case Sensetive" + "<br>")
+document.write("Variable names should not be JS Keywords" + "<br>")
+
+
+
+
+
