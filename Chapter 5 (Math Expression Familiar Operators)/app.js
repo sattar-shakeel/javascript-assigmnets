11d9c4900fcbfc61bebb604a9e423e071defb81a
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-22 01:52:27 +0500

    Add files via upload

---
+++ Chapter 5 (Math Expression Familiar Operators)/app.js
@@ -0,0 +1,112 @@
+// Question 1
+
+// let num1 = 3;
+// let num2 = 5;
+
+// let sum = num1 + num2
+// document.write("Sum of 3 and 5 is " + sum + "<br>")
+
+
+
+// // Question 2
+
+// let sub = num1 - num2
+// document.write("Subtraction of 3 and 5 is " + sub + "<br>") 
+
+
+// let mul = num1 * num2
+// document.write("Multiply of 3 and 5 is " + mul + "<br>")
+
+// let div = num1 / num2
+// document.write("Division of 3 and 5 is " + div + "<br>")
+
+// let mod = num1 % num2
+// document.write("Modulus of 3 and 5 is " + mod + "<br>")
+
+
+
+// Question 4
+
+// let ticketCost = 600
+// let ticketQuantity = 5
+
+// let totalAmount = ticketCost*5
+
+// document.write("Total cost to buy 5 tickets to a movie is " + totalAmount + "PKR")
+
+
+
+// Question 5
+
+// let userInput = +prompt("Enter a number")
+
+// document.write("Table of " + userInput + "<br>")
+// document.write("<br>")
+// document.write(userInput + " x " + " 1 " + " = " + userInput*1 + "<br>")
+// document.write(userInput + " x " + " 2 " + " = " + userInput*2 + "<br>")
+// document.write(userInput + " x " + " 3 " + " = " + userInput*3 + "<br>")
+// document.write(userInput + " x " + " 4 " + " = " + userInput*4 + "<br>")
+// document.write(userInput + " x " + " 5 " + " = " + userInput*5 + "<br>")
+// document.write(userInput + " x " + " 6 " + " = " + userInput*6 + "<br>")
+// document.write(userInput + " x " + " 7 " + " = " + userInput*7 + "<br>")
+// document.write(userInput + " x " + " 8 " + " = " + userInput*8 + "<br>")
+// document.write(userInput + " x " + " 9 " + " = " + userInput*9 + "<br>")
+// document.write(userInput + " x " + " 10 " + " = " + userInput*10 + "<br>")
+// document.write(userInput + " x " + " 11 " + " = " + userInput*11 + "<br>")
+// document.write(userInput + " x " + " 12 " + " = " + userInput*12 + "<br>")
+// document.write(userInput + " x " + " 13 " + " = " + userInput*13 + "<br>")
+// document.write(userInput + " x " + " 14 " + " = " + userInput*14 + "<br>")
+// document.write(userInput + " x " + " 15 " + " = " + userInput*15 + "<br>")
+// document.write(userInput + " x " + " 16 " + " = " + userInput*16 + "<br>")
+// document.write(userInput + " x " + " 17 " + " = " + userInput*17 + "<br>")
+// document.write(userInput + " x " + " 18 " + " = " + userInput*18 + "<br>")
+// document.write(userInput + " x " + " 19 " + " = " + userInput*19 + "<br>")
+// document.write(userInput + " x " + " 20 " + " = " + userInput*20 + "<br>")
+
+
+
+// Question 7
+
+// let price1 = +prompt("Enter a price of item 1")
+// let price2 = +prompt("Enter a Price of item 2")
+// let quantity1 = +prompt("Enter a Quantity of item 1")
+// let quantity2 = +prompt("Enter a Quantity of item 2")
+// let shippingCharges = +prompt("Enter a Shipping Charges")
+
+// document.write("<h1>Shopping Cart</h1>" + "<br>")
+// document.write("Price of item 1 is " + price1 + "<br>")
+// document.write("Quantity of item 1 is " + quantity1 + "<br>")
+// document.write("Price of item 2 is " + price2 + "<br>")
+// document.write("Quantity of item 2 is " + quantity2 + "<br>")
+// document.write("Shipping Charges " + shippingCharges + "<br>")
+
+// let totalAmount = price1*quantity1+price2*quantity2+shippingCharges
+// document.write("total cost of your order is " + totalAmount )
+
+
+
+// Question 8
+
+// let obtainMarks = +prompt("Enter your obtain marks")
+// let totalMarks = +prompt("Enter your total marks")
+
+// let percentage = obtainMarks/totalMarks*100
+
+// document.write("<h1>Mark Sheet</h1>")
+// document.write("Total Marks: " + totalMarks + "<br>")
+// document.write("Obtain Marks: " + obtainMarks + "<br>")
+// document.write("Percentage: " + percentage + "%" + "<br>")
+
+
+
+// Question 9
+
+// let usDollars = +prompt("Enter US Dollar")
+// let saudi = +prompt("Enter saudi Dollar")
+// let pkr = +prompt("Enter PKR")
+
+// let dollar_to_pkr =    
+
+
+
+
