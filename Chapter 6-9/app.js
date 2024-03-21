93d9bb60f460f531080d113331558af4b8a27f81
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-22 01:53:07 +0500

    Add files via upload

---
+++ Chapter 6-9/app.js
@@ -0,0 +1,88 @@
+
+// Question 1
+// var a = 10
+
+// var num1 = ++a
+// var num1 = a++
+// // var num1 = --a
+// // var num1 = a--
+
+// document.write("The Value of a is: 10" + "</br>")
+// document.write("................................" + "</br>")
+// document.write("</br>")
+
+// document.write("The Value of ++a is: " + num1 + "</br>")
+// document.write("Now the Value of a is: " + num1 + "</br>")
+// document.write("</br>")
+
+
+// document.write("The Value of a++ is: " + num1 + "</br>")
+// document.write("Now the Value of a is: " + num1 + "</br>")
+// document.write("</br>")
+
+// document.write("The Value of --a is: " + num1 + "</br>")
+// document.write("Now the Value of a is: " + num1 + "</br>")
+// document.write("</br>")
+
+// document.write("The Value of --a is: " + num1 + "</br>")
+// document.write("Now the Value of a is: " + num1 + "</br>")
+// document.write("</br>")
+
+
+
+// Question 2
+
+// let a = 2
+// let b = 1
+
+// document.write("a is " + a + "<br>")
+// document.write("b is " + b + "<br>" )
+
+// let result = --a - --b + ++b + b--;
+// document.write("Result is " + result+ "<br>" )
+
+
+
+// Question 3
+
+// let userName = prompt("enter your name")
+// document.write("welcome " + userName)
+
+
+
+// Question 5
+// let num = +prompt("Enter a Number" , 5)
+
+
+// document.write(num  + " x " + 1 + " = " + num*1 + "<Br>" )
+// document.write(num  + " x " + 2 + " = " + num*2 + "<Br>" )
+// document.write(num  + " x " + 3 + " = " + num*3 + "<Br>" )
+// document.write(num  + " x " + 4 + " = " + num*4 + "<Br>" )
+// document.write(num  + " x " + 5 + " = " + num*5 + "<Br>" )
+// document.write(num  + " x " + 6 + " = " + num*6 + "<Br>" )
+// document.write(num  + " x " + 7 + " = " + num*7 + "<Br>" )
+// document.write(num  + " x " + 8 + " = " + num*8 + "<Br>" )
+// document.write(num  + " x " + 9 + " = " + num*9 + "<Br>" )
+// document.write(num  + " x " + 10 + " = " + num*10 + "<Br>" )
+
+
+
+// Question 6
+
+let sub1 = prompt("Enter Subject 1")
+let sub2 = prompt("Enter Subject 2")
+let sub3 = prompt("Enter Subject 3")
+let totalMarks = 100
+let obatinedMarks1 = prompt("Enter Your Obtained Marks")
+let obatinedMarks2 = prompt("Enter Your Obtained Marks")
+let obatinedMarks3 = prompt("Enter Your Obtained Marks")
+
+
+// document.write("<h1>Subject Total Marks Obtained Marks Percentage </h1>")
+// document.write(sub1 + totalMarks + obatinedMarks1 + obatinedMarks1*totalMarks/100 +"%"+ "<br>")
+// document.write(sub2 + totalMarks + obatinedMarks2 + obatinedMarks2*totalMarks/100 +"%"+ "<br>")
+// document.write(sub3 + totalMarks + obatinedMarks3 + obatinedMarks3*totalMarks/100 +"%"+ "<br>")
+
+document.write("<table><tr><th>Subject</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr></table>")
+document.write("<table><tr><td>sub1</td><td>totalMarks</td><td>obatinedMarks1</td><td>obatinedMarks1/totalMarks*100</td></tr></table>")
+
