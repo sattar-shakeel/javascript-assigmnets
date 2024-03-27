1eac8b0f2c16c3489749b076382f4300e403b390
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-28 01:51:03 +0500

    Add files via upload

---
+++ Chapter 9-11/app.js
@@ -0,0 +1,179 @@
+
+// Question 1
+
+// let city = prompt("Enter Your City Name")
+// if (city === "karachi"){
+//     console.log("Welcome to city of lights")
+// }
+// else{
+//     console.log("city of pakistan")
+// }
+
+
+
+// Question 2
+
+// let gender = prompt("Enter Your Gender")
+// if(gender === "male"){
+//     document.write("Good Morning Sir")
+// }
+// else if(gender === "female"){
+//     document.write("Good Morning Mam")
+// }
+// else{
+//     document.write("Please Enter Gender")
+// }
+
+
+
+// Question 3
+
+// let trafficLight = prompt("Enter Traffic Light Color")
+// if(trafficLight === "red"){
+//     console.log("Must Stop")
+// }
+// else if(trafficLight === "yellow"){
+//     console.log("Ready To Mave")
+// }
+// else if( trafficLight === "green"){
+//     console.log("Move Now")
+// }
+// else{
+//     console.log("Enter Correct Color")
+// }
+
+
+
+// Question 4
+
+// let fuel = prompt("Enter Fuel in Liter")
+// if(fuel <= 0.25){
+//     console.log("Please refill the fuel in your car")
+// }
+// else{
+//     console.log("bhot petrol hai")
+// }
+
+
+// Question 6
+
+// let num1 = +prompt("enter obatained Marks of Subject 1");
+// let num2 = +prompt("enter obatained Marks of Subject 2");
+// let num3 = +prompt("enter obatained Marks of Subject 3");
+// let obtainedMarks = num1 + num2 + num3;
+// let totalMarks = prompt("Enter Total Marks")
+// let percentage = (obtainedMarks/totalMarks*100)
+// document.write("Total Marks : " + totalMarks + "</br>")
+// document.write("Obtained Marks : " + obtainedMarks + "</br>" )
+// document.write("percentage : " + percentage +"%" + "</br>" )
+
+// if(percentage >= 80){
+//     document.write("Grade : A-one" + "</br>")
+//     document.write("Remarks : Excellent" + "</br>")
+// }
+
+// else if(percentage >= 70){
+//     document.write("Grade : A" + "</br>")
+//     document.write("Remarks : Good" + "</br>")
+// }
+
+// else if(percentage >= 60){
+//     document.write("Grade : B" + "</br>")
+//     document.write("Remarks : You Need To Improve" + "</br>")
+// }
+
+// else if     (percentage < 60){
+//     document.write("Grade : Fail" + "</br>")
+//     document.write("Remarks : sorry" + "</br>")
+// }
+
+
+
+// Question 7
+
+// let secretNumber = 6
+// let guessNumber = +prompt("Enter Secret Number")
+
+// if(secretNumber === guessNumber){
+//     document.write("Bingo! Correct answer")
+// }
+
+// else if(++secretNumber === guessNumber ){
+//     document.write("Close enough to the correct answer")
+// }
+
+// else{
+//     document.write("Wrong Answer")
+// }
+
+
+
+// Question 8
+
+// let num = 3
+
+// if(num === 0){
+//     document.write(num + " is divisible by 3.")
+// }
+// else{
+//     document.write(num + " is not divisible by 3.")
+// }
+
+
+
+// Question 10
+
+// let temperature = +prompt("Enter Temperature")
+
+// if(temperature > 40){
+//     document.write("It is too hot outside.")
+// }
+
+// else if(temperature > 30){
+//     document.write("The Weather today is Normal.")
+// }
+
+// else if(temperature > 20){
+//     document.write("Today's Weather is Cool.")
+// }
+
+// else if(temperature > 10){
+//     document.write("OMG! Today’s weather is so Cool.")
+// }
+
+
+
+// Question 11
+
+// let num1 = 2
+// let num2 = 3
+
+// let sum = num1 + num2
+
+// if(sum){
+//     document.write(num1 + " + " + num2 + " = " + sum + "</br>")
+// }
+
+// let sub = num1 - num2
+
+// if(sub){
+//     document.write(num1 + " - " + num2 + " = " + sub + "</br>")
+// }
+
+// let multiply = num1 * num2
+
+// if(multiply){
+//     document.write(num1 + " x " + num2 + " = " + multiply + "</br>")
+// }
+
+// let devide = num1 / num2
+
+// if(devide){
+//     document.write(num1 + " / " + num2 + " = " + devide + "</br>")
+// }
+
+// let modules = num1 % num2
+
+// if(modules){
+//     document.write(num1 + " % " + num2 + " = " + modules + "</br>")
+// }
