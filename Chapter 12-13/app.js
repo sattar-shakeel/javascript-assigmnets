755a04b9a1e53a5e04bed19da2fbe19132b217f5
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-03-30 02:57:46 +0500

    Add files via upload

---
+++ Chapter 12-13/app.js
@@ -0,0 +1,117 @@
+
+// Question 1
+
+// let asciiValue = +prompt("Enter a ASCII Value")
+// let A = 65
+// let Z = 90
+// let a = 97
+// let z = 122
+
+
+// if(asciiValue >= 48 && asciiValue <= 57){
+//     console.log("Number")
+// }
+
+// else if(asciiValue >= 65 && asciiValue <= 90){
+//     console.log("Upper Case")
+// }
+
+// else if(asciiValue >= 97 && asciiValue <= 122){
+//     console.log("Lower Case")
+// }
+
+// else{
+//     console.log("Unknown")
+// }
+
+
+
+// Question 2
+
+// let num1 = +prompt("enter num 1")
+// let num2 = +prompt("enter num 2")
+
+// if(num1 > num2){
+//     console.log(num1 + " is greater then " + num2)
+// }
+// else if (num2 > num1){
+//     console.log(num2 + " is greater then " + num1)
+// }
+// else{
+//     console.log("Both Num arey equal")
+// }
+
+
+
+// Question 3
+
+// let num = +prompt("enter a num")
+
+// if(num > 0){
+//     console.log(num + " is positive")
+// }
+// else if(num < 0){
+//     console.log(num + " is negative")
+// }
+// else{
+//     console.log("Num is Zero")
+// }
+
+
+
+// Question 4
+
+// let userVowel = prompt("enter a vowel")
+
+// if(userVowel === "a"||userVowel === "e"||userVowel === "i"||userVowel === "o"||userVowel === "u" ){
+//     console.log("true")
+// }
+// else{
+//     console.log("false")
+// }
+
+
+
+// // Question 5
+
+// let dbPassword = "sattarshakeel"
+// let pass = prompt("enter your password")
+
+// if(pass === ""){
+//     console.log("Please Enter your Password")
+// }
+// else if(dbPassword === pass){
+//     console.log("correct!")
+// }
+// else{
+//     console.log("Incorect Password")
+// }
+
+
+
+// Question 6
+
+// let hour = 13
+
+// if(hour < 18){
+//     console.log("Good Day")
+// }
+// else[
+//     console.log("Good Evening")
+// ]
+
+
+
+// Question 7
+
+// let time = +prompt("time")
+
+// if(time >= 0 && time <=1200){
+//     console.log(time + " AM")
+// }
+// else if(time >= 1200 && time <= 2400 ){
+//     console.log(time + " PM")
+// }
+// else{
+//     console.log("Please enter a time between 0000 and 2359.")
+// }
