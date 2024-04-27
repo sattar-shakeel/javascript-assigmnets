91e79bba637dc5dacd8f067301f7ab5d4af53fa3
sattar shakeel <154556813+sattar-shakeel@users.noreply.github.com>
2024-04-28 01:53:52 +0500

    Add files via upload

---
+++ Chapter 14-16/app.js
@@ -0,0 +1,103 @@
+
+// Question 1
+
+// let studentName = [];
+
+
+// Question 2
+
+// let studentName {
+//     names []
+// }
+
+
+// Question 3
+
+// let stringArray = ["january , february , march , april"]
+
+
+// Question 4
+
+// let numberArrey = [20 , 30 , 40 , 50]
+
+
+// Question 5
+
+// let booleanArrey = [true , false]
+
+
+// Question 6
+
+// let mixedArrey = ["sattar" , 20 , true]
+
+
+// Question 7
+
+// let educationQualification = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","Phd"]
+
+// for (let i=0; i<=educationQualification.length-1; i++){
+//     document.write(educationQualification[i]+"<br/>")
+// }
+
+
+
+// Question 8
+
+// let studentName =["Mitcheal " , "John " , "Tony "]
+// let studentMarks =[320 , 230 , 480]
+
+// for (let i=0; i<=studentName.length-1; i++){
+//     document.write("Score of " +studentName[i] + studentMarks[i] + " Percentage is " + studentMarks[i]/500*100 +"%" + "<br>" )
+// }
+
+
+
+// Question 10
+
+// let studentsScore =[320, 230, 480, 120]
+// document.write("Scores of Students : " + studentsScore+ "<br>")
+// document.write("Ordered Scores of Students : " + studentsScore.sort())
+
+
+
+// Question 11
+
+// let cities = ["Karachi" , "Lahore" , " Islamabad" , " Quetta" , "Peshawar"]
+
+// document.write("Cities List:" + "<br>" + cities + "<br>")
+// document.write("Selected Cities List: " +"<br>" + cities[2]+cities[3])
+
+
+
+// Question 12
+
+// let joinMethod = [" This " , " is " , " my " , " Cat "]
+
+// document.write("Arrey:" + joinMethod + "<br>")
+// document.write("String:" + joinMethod.join(" "))
+
+
+
+// Question 13
+
+// let devices = [" Keyboard " , " Mouse " , " Printer " , " Monitor "]
+
+// document.write("Devices: " + "<br>" + devices +"<br>")
+
+// for (i=0 ; i<=devices.length-1 ; i++)
+// document.write("Out:"+"<br>" + devices[i] + "<br>")
+
+
+
+
+// Question 14
+
+// let devices = [" Keyboard " , " Mouse " , " Printer " , " Monitor "]
+
+// document.write("Devices: " + "<br>" + devices +"<br>")
+
+// for (i=devices.length-1 ; i>=0 ; i--)
+// document.write("Out:" + "<br>" +devices[i] + "<br>")
+
+
+
